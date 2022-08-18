27f166c25d0b6814c380a01b695b5adee9b0c2a6
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2022-08-18 17:45:58 +0200

    Some more debugging options

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -26,9 +26,9 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "line",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- e2e/tests/helpers.ts
+++ e2e/tests/helpers.ts
@@ -9,6 +9,26 @@ export function runCmd(cmd: string, opt?: SpawnOptionsWithoutStdio) {
       shell: true,
     });
 
+    if (child.stdout) {
+      console.log("has stdout");
+      child.stdout.setEncoding("utf8");
+      child.stdout.on("data", function (data) {
+        console.log("stdout: " + data);
+      });
+    }
+
+    if (child.stderr) {
+      console.log("has stderr");
+      child.stderr.setEncoding("utf8");
+      child.stderr.on("data", function (data) {
+        console.log("stderr: " + data);
+      });
+    }
+
+    child.on("close", (code) => {
+      console.log("closed", code);
+    });
+
     child.on("close", (code) => {
       if (code !== 0) {
         reject(`Command \"${executable} ${args.join(" ")}\" failed`);
