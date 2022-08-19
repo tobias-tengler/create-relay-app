14ba140945e69950cd8f5543f8a1699bc3a29dfb
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2022-08-19 16:36:00 +0200

    Instantly fail if one test fails

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -22,7 +22,8 @@ const config: PlaywrightTestConfig = {
     timeout: 5000,
   },
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
+  maxFailures: 1,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
